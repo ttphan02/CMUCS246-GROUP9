af5109159be512c4722d014bfd476a7d59932c3e
ttphan02 <phantruc0210@gmail.com>
2024-04-03 21:10:58 +0700

    CIAm

---
+++ CIMA/src/main/java/com/mycompany/cima/CIMA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cima;
+
+/**
+ *
+ * @author HP
+ */
+public class CIMA {
+    public static void main(String[] args) {
+        Home home = new Home();
+        home.setVisible(true);
+    }
+}
