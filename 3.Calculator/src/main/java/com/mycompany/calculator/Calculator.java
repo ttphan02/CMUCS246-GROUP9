c942e4bbbc3ede01b0c569968cede07e2af9dd7d
ttphan02 <phantruc0210@gmail.com>
2024-05-24 00:21:07 +0700

    Submit

---
+++ 3.Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author HP
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        FormMain fm = new FormMain();
+        fm.setVisible(true);
+    }
+}
